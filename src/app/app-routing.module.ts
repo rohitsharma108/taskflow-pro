2d2a913994297c87a85ad0ca615be5757480b33e
Rohit Sharma <imrohit10800@gmail.com>
2026-01-14 12:11:10 +0530

    feat: lazy-loaded routing for feature modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'auth',
+    pathMatch: 'full'
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./features/auth/auth.module')
+        .then(m => m.AuthModule)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./features/dashboard/dashboard.module')
+        .then(m => m.DashboardModule)
+  },
+  {
+    path: 'projects',
+    loadChildren: () =>
+      import('./features/projects/projects.module')
+        .then(m => m.ProjectsModule)
+  },
+  {
+    path: 'tasks',
+    loadChildren: () =>
+      import('./features/tasks/tasks.module')
+        .then(m => m.TasksModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'auth'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
